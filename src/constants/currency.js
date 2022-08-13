4075429eee1ceee9d7e06523b73b9bcbc60ad212
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-08-13 13:39:54 +0200

    create currency constant file

---
+++ src/constants/currency.js
@@ -0,0 +1,22 @@
+import dollar from '../assets/images/icons/currency/dollar.svg';
+import euro from '../assets/images/icons/currency/euro.svg';
+import yen from '../assets/images/icons/currency/yen.svg';
+
+const CURRENCY = [
+	{
+		id: 'dollar',
+		label: 'USD',
+		icon: dollar,
+	},
+	{
+		id: 'euro',
+		label: 'EUR',
+		icon: euro,
+	},
+	{
+		id: 'yen',
+		label: 'JPY',
+		icon: yen,
+	},
+];
+export default CURRENCY;
