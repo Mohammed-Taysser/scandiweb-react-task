eea360f1310b77c6865f919b7fe99f9668e02e8d
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-08-13 22:59:29 +0200

    refactor to class base component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './layout/Navbar';
 import Routes from './routes';
 import './assets/scss/core.scss';
 
-function App() {
-	return (
-		<>
-			<Navbar />
-			<Routes />
-		</>
-	);
+class App extends Component {
+	render() {
+		return (
+			<>
+				<Navbar />
+				<Routes />
+			</>
+		);
+	}
 }
 
 export default App;
