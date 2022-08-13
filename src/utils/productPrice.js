ae7903e4a316722defe5d8fdc8a7e6d8e9c20a83
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-08-13 18:00:13 +0200

    rename utils name

--- src/utils/productPrice.js
+++ src/utils/productPrice.js
@@ -1,6 +1,6 @@
-function useProductPrice(price=[]) {
+function productPrice(price = []) {
 	const selectedPrice = price.find((value) => value.currency.label === 'USD');
 	return selectedPrice.amount;
 }
 
-export default useProductPrice;
+export default productPrice;
