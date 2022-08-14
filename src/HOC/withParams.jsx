738442559e5a13971f6cdd89297ba28af32d6ea7
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-08-14 13:39:04 +0200

    move to HOC directory

--- src/HOC/withParams.jsx
+++ src/HOC/withParams.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 /**
- * wrap react-router-dom params into class base component
+ * HOC to wrap react-router-dom params into class base component
  * @see https://github.com/remix-run/react-router/issues/8146#issuecomment-947860640
+ * @see https://infinum.com/blog/how-to-use-react-hooks-in-class-components/
  * @param {*} Component
  * @returns JSX
  */
-function withParams(Component) {
+function withParamsHOC(Component) {
 	return (props) => <Component {...props} params={useParams()} />;
 }
 
-export default withParams;
+export default withParamsHOC;
