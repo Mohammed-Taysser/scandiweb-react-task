79309384424d12c1fe4faedada0492fb9b2d9540
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-08-14 08:55:47 +0200

    create cart utils

---
+++ src/utils/cart.js
@@ -0,0 +1,27 @@
+import { exchangePrice } from './exchange';
+/**
+ * calculate cart items length
+ * @param {Array} cart cart items
+ * @returns {Number} items length
+ */
+function cartItemsLength(cart = []) {
+	return cart.reduce((prev, current) => prev + current.quantity, 0);
+}
+
+/**
+ * calculate cart total fees
+ * @param {Array} cart cart items
+ * @param {Object} currency current currency
+ * @returns {Number} fees
+ */
+function cartTotalFees(cart = [], currency = {}) {
+	return cart
+		.reduce(
+			(prev, current) =>
+				prev + current.quantity * exchangePrice(current.prices, currency),
+			0
+		)
+		.toFixed(2);
+}
+
+export { cartItemsLength, cartTotalFees };
