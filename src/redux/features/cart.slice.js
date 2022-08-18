b4d99d7b093be0a4150520a7b8133277a5ce0852
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-08-18 10:59:42 +0200

    add `updateCartItem` reducer

--- src/redux/features/cart.slice.js
+++ src/redux/features/cart.slice.js
@@ -10,10 +10,20 @@ export const cartSlice = createSlice({
 			state.items.push(action.payload);
 			localStorage.setItem('cart', JSON.stringify(state.items));
 		},
+		updateCartItem: (state, action) => {
+			const updatedCartItems = state.items.map((item) => {
+				if (item.id === action.payload.id) {
+					return { ...item, ...action.payload.item };
+				}
+				return item;
+			});
+			state.items = updatedCartItems;
+			localStorage.setItem('cart', JSON.stringify(updatedCartItems));
+		},
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const { addCartItem } = cartSlice.actions;
+export const { addCartItem, updateCartItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
