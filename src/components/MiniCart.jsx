731ccfab17ea8705cbfe1c113ccefce7d5da99c0
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-10-21 13:28:31 +0200

    remove unused `link` import

--- src/components/MiniCart.jsx
+++ src/components/MiniCart.jsx
@@ -1,6 +1,5 @@
 import React, { Component, createRef } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { cartTotalFees } from '../utils/cart';
 import CartList from './CartList';
 import Alert from './Alert';
